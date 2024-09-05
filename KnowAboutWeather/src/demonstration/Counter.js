2d566f5c19a954996a6cbbd4742665ef40a2d387
Aathish R Viswam <aathishrv@gmail.com>
2024-09-06 00:47:36 +0530

    get current geolocation

--- KnowAboutWeather/src/demonstration/Counter.js
+++ KnowAboutWeather/src/demonstration/Counter.js
@@ -1,29 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 const Counter = () => {
-  let count = 0
+  const [count, setCount] = useState(0) //useState understanding
+  const [newcount, setNewCount] = useState(0) //useState understanding
+  useEffect(
+    () => {
+      console.log(`Our count changed`)
+      console.log(`useEffect CleanUp`)
+    },
+    [count], //dependency array
+  )
   return (
-    <View>
-      <Text>{`count : ${count}`}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{`count : ${count}`}</Text>
       <Button
         color={'red'}
         title={'Increase the count'}
         onPress={() => {
-          count++
-          console.log(count)
+          setCount(count + 1)
         }}
       />
       <Button
         color={'green'}
         title={'Decrease the count'}
         onPress={() => {
-          count--
-          console.log(count)
+          setCount(count - 1)
+        }}
+      />
+      <Button
+        color={'red'}
+        title={'Increase the count'}
+        onPress={() => {
+          setNewCount(count + 1)
+        }}
+      />
+      <Button
+        color={'green'}
+        title={'Decrease the count'}
+        onPress={() => {
+          setNewCount(count - 1)
         }}
       />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 25,
+    marginTop: 25,
+  },
+})
 export default Counter
